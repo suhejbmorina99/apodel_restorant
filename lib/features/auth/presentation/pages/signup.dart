fd5c8e64ecdd3b798c1efc49852709e47b3db5a6
Suhejb <suhejbmorina@gmail.com>
2026-01-23 22:18:50 +0100

    start working on signup screen

---
+++ lib/features/auth/presentation/pages/signup.dart
@@ -0,0 +1,271 @@
+import 'package:apodel_restorant/features/auth/presentation/pages/login.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_fonts/google_fonts.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+
+// import 'package:stretchandmobility/services/auth.dart';
+
+class SignupScreen extends StatelessWidget {
+  SignupScreen({super.key});
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      resizeToAvoidBottomInset: true,
+      bottomNavigationBar: _signin(context),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Join Str8ch Today!',
+                    style: GoogleFonts.nunito(
+                      textStyle: TextStyle(
+                        color: Theme.of(context).colorScheme.onPrimary,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 80),
+              _emailAddress(context),
+              const SizedBox(height: 20),
+              _password(context),
+              const SizedBox(height: 32),
+              _signup(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _emailAddress(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Email Address',
+          style: GoogleFonts.nunito(
+            textStyle: TextStyle(
+              color: Theme.of(context).colorScheme.onPrimary,
+              fontWeight: FontWeight.normal,
+              fontSize: 16,
+            ),
+          ),
+        ),
+        const SizedBox(height: 16),
+        SizedBox(
+          width: MediaQuery.of(context).size.width - 50,
+          height: 50,
+          child: TextField(
+            controller: _emailController,
+            decoration: InputDecoration(
+              filled: true,
+              hintText: 'str8ch@hotmail.com',
+              hintStyle: GoogleFonts.nunito(
+                color: Theme.of(context).colorScheme.secondary,
+                fontWeight: FontWeight.normal,
+                fontSize: 14,
+              ),
+              fillColor: Theme.of(context).colorScheme.primaryContainer,
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(14),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(14),
+                borderSide: BorderSide(
+                  color: Theme.of(context).colorScheme.onSurfaceVariant,
+                  width: 2,
+                ),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(14),
+                borderSide: const BorderSide(
+                  color: Color.fromARGB(255, 253, 199, 69),
+                  width: 2,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _password(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Password',
+          style: GoogleFonts.nunito(
+            textStyle: TextStyle(
+              color: Theme.of(context).colorScheme.onPrimary,
+              fontWeight: FontWeight.normal,
+              fontSize: 16,
+            ),
+          ),
+        ),
+        const SizedBox(height: 16),
+        SizedBox(
+          width: MediaQuery.of(context).size.width - 50,
+          height: 50,
+          child: TextField(
+            obscureText: true,
+            controller: _passwordController,
+            decoration: InputDecoration(
+              filled: true,
+              hintText: '********',
+              hintStyle: GoogleFonts.nunito(
+                color: Theme.of(context).colorScheme.secondary,
+                fontWeight: FontWeight.normal,
+                fontSize: 14,
+              ),
+              fillColor: Theme.of(context).colorScheme.primaryContainer,
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(14),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(14),
+                borderSide: BorderSide(
+                  color: Theme.of(context).colorScheme.onSurfaceVariant,
+                  width: 2,
+                ),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(14),
+                borderSide: const BorderSide(
+                  color: Color.fromARGB(255, 253, 199, 69),
+                  width: 2,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _signup(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width - 50,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: () async {
+          HapticFeedback.lightImpact();
+          final email = _emailController.text.trim();
+          final password = _passwordController.text;
+
+          FocusScope.of(context).unfocus();
+
+          // Basic email format validation
+          if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(email)) {
+            Fluttertoast.showToast(
+              msg: "Please enter a valid email address.",
+              backgroundColor: Colors.black54,
+              textColor: Colors.white,
+            );
+            return;
+          }
+
+          // Password length validation
+          if (password.length < 6) {
+            Fluttertoast.showToast(
+              msg: "Password must be at least 6 characters long.",
+              backgroundColor: Colors.black54,
+              textColor: Colors.white,
+            );
+            return;
+          }
+
+          // bool success = await AuthService().signup(
+          //   email: email,
+          //   password: password,
+          //   context: context,
+          // );
+
+          // if (success) {
+          //   final SharedPreferences preferences =
+          //       await SharedPreferences.getInstance();
+          //   await preferences.setInt(
+          //     'lastLoginTimestamp',
+          //     DateTime.now().millisecondsSinceEpoch,
+          //   );
+
+          //   await preferences.setBool('isGuest', false);
+          // }
+        },
+        style: ElevatedButton.styleFrom(
+          foregroundColor: const Color.fromARGB(255, 33, 33, 33),
+          backgroundColor: const Color.fromARGB(255, 253, 199, 69),
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        child: Text(
+          'Sign Up',
+          style: GoogleFonts.nunito(
+            fontSize: 18,
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _signin(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 32),
+      child: RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: "Already Have Account? ",
+              style: GoogleFonts.nunito(
+                color: Theme.of(context).colorScheme.secondary,
+                fontWeight: FontWeight.normal,
+                fontSize: 16,
+              ),
+            ),
+            TextSpan(
+              text: "Log In",
+              style: GoogleFonts.nunito(
+                color: Theme.of(context).colorScheme.onPrimary,
+                fontWeight: FontWeight.normal,
+                fontSize: 16,
+              ),
+              recognizer: TapGestureRecognizer()
+                ..onTap = () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()),
+                  );
+                },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
