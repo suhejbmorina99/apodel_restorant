aac8365d4d0a53eb664d80bbb0ce66627fe18789
Suhejb <suhejbmorina@gmail.com>
2026-01-31 20:15:20 +0100

    created processing information

---
+++ lib/features/registration/presentation/widgets/processing_information.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ProcessingInformation extends StatelessWidget {
+  const ProcessingInformation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
